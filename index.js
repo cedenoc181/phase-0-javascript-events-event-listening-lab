32bf701b7de530c4392b851e75662b956ec933f9
Christian <cedenoc181@gmail.com>
2022-09-05 20:40:24 -0400

    commit tree a5e4ec0daf7cc1643874c20f14c415f98b14a1be

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById('button');{
+        input.addEventListener('', function(){
+            alert('I was clicked');
+        })
+    };
 }
+
+
+// const input = document.getElementById('main');{
+//     input.addEventListener('clickj', function(){
+//         alert('I was clicked');
+//     })
+// };
+
